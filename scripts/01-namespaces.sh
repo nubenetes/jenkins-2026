28500af2a0d5e7f6714f645aec301e1ce39fa58f
inafev <inafev@users.noreply.github.com>
2026-06-17 20:58:40 +0200

    perf(gke): scale up ResourceQuota in jenkins namespace and upgrade node pool to e2-standard-8

--- scripts/01-namespaces.sh
+++ scripts/01-namespaces.sh
@@ -198,10 +198,10 @@ metadata:
   name: jenkins-quota
 spec:
   hard:
-    requests.cpu: "8.0"
-    requests.memory: 20.0Gi
-    limits.cpu: "30.0"
-    limits.memory: 40.0Gi
+    requests.cpu: "16.0"
+    requests.memory: 32.0Gi
+    limits.cpu: "60.0"
+    limits.memory: 64.0Gi
 EOF
 
 # 2. Observability Namespace Quota
